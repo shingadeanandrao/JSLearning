d6a28e912d0b4f7a748e2188f4761902e4c1f493
shingadeanandrao <shingadeanandrao@gmail.com>
2023-09-21 21:30:15 +0530

    Variables Assignment
    Variables Assignment

---
+++ 01_variable.js
@@ -0,0 +1,13 @@
+var firstName; //variable declaration
+firstName="Anandrao";//variable initialization
+console.log(firstName);
+firstName="Mr.Anandrao";//variable update
+console.log(firstName);
+
+var lastName="Shingade";// variable declaration and initialization
+console.log(lastName);
+
+var height;           //variable declaration
+console.log(height);
+height= "6"
+console.log(height); //variable initialization
---
+++ 01_variableAssign.js
@@ -0,0 +1,23 @@
+var myFullName; //variable declaration
+console.log(myFullName);
+myFullName="Anandrao Shingade";//variable initialization
+console.log(myFullName);
+myFullName="Anandrao Sambhaji Shingade"; //update
+console.log(myFullName);
+
+var myLove = 'I Love only JS';
+console.log(myLove);
+var firstName="Anandrao";
+console.log("First name:",firstName);
+var lastName="Shingade";
+console.log("Last name:",lastName);
+var age="27";
+console.log("Age is:",age);
+var address="Hadapsar,Pune";
+console.log("My Address is::",address);
+var pinCode="411028";
+console.log("Pincode:",pinCode);
+var gender="Male";
+console.log("Gender:",gender);
+var isMarried="No";
+console.log("Marital Status:",isMarried);
