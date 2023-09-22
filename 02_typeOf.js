9dac813395d68e03fe4246f1154e2bb88f65b588
shingadeanandrao <shingadeanandrao@gmail.com>
2023-09-22 21:10:14 +0530

    Variable,datatype and typeof operator
    Variable, datatype and typeof operator

---
+++ 02_typeOf.js
@@ -0,0 +1,24 @@
+console.log("========Assignment 02========");
+console.log("========Step 1=====================");
+var bankName;
+console.log(" Before Initialization--> Bank Name:-",bankName,"    ","Variable Type:-",typeof bankName);
+bankName='SBI Bank';
+console.log(" Before Initialization--> Bank Name:-",bankName,"     ","Variable Type:-",typeof bankName);
+console.log("========Step 2====================");
+var marks='90%';
+console.log("Marks:-",marks,"    ","Variable type:-",typeof(marks));
+console.log("========Step 3====================");
+var isWorking;
+console.log(" Before Initialization--> IsWorking:-",isWorking,"    ","Variable Type:-",typeof isWorking);
+isWorking=true;
+console.log(" After Initialization--> IsWorking:-",isWorking,"          ","Variable Type:-",typeof isWorking);
+console.log("========Step 3===================");
+var totalCount="Hundred and Seven";
+console.log("Total Count:-",totalCount,"                    ","Variable type:-",typeof(totalCount));
+totalCount=107;
+console.log("Total Count:-",totalCount,"                                  ","Variable type:-",typeof(totalCount));
+
+
+
+
+
