6ea6a70a62c0b548958f955b5596bcf347059605
shingadeanandrao <shingadeanandrao@gmail.com>
2023-09-22 19:59:50 +0530

    Primitive datatypes
    Primitive datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+var firstName="Anandrao";
+console.log(firstName);
+console.log(typeof firstName);
+var hscMarks='79';
+console.log(hscMarks, typeof(hscMarks));
+var pinCode=415509;
+console.log(pinCode,typeof(pinCode));
+
+var isMarried=true;
+console.log(isMarried,typeof(isMarried));
+
+console.log(typeof("I Love Only JS"));
+console.log(typeof(87654));
+console.log(typeof(false));
---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,37 @@
+console.log("**************Assignment 01*****************");
+console.log("===================Part 01=================");
+console.log("========String Variables=========");
+var firstName='Anandrao';
+console.log('First Name:-',firstName,"      " , "Variable Type:-",typeof(firstName));
+var lastName='Shingade';
+console.log('Last Name:-',lastName,"       " , "Variable Type:-",typeof(lastName));
+var address='Pune';
+console.log('Address:-',address,"             " , "Variable Type:-",typeof(address));
+
+console.log("========Number Variables=========");
+var pinCode=415509;
+console.log('Pincode:-',pinCode,"            " , "Variable Type:-",typeof(pinCode));
+var streetNumber=35;
+console.log('Street Number:-',streetNumber,"          " , "Variable Type:-",typeof(streetNumber));
+var distance=25.5;
+console.log('Distance:-',distance,"             " , "Variable Type:-",typeof(distance));
+
+console.log("========Boolean Variables=========");
+var isAvailable=true;
+console.log('Availabality Status:-',isAvailable,"        " , "Variable Type:-",typeof(isAvailable));
+var isEmployed=true;
+console.log('Employability Status:-',isEmployed,"       " , "Variable Type:-",typeof(isEmployed));
+var isSick=false;
+console.log('Health Condition:-',isSick,"          " , "Variable Type:-",typeof isSick);
+
+console.log("=======Difference between null and undefined=======");
+console.log('================Null=====================')
+var amount=null;
+console.log('Amount:-',amount,"           ","Variable Type:-", typeof amount);
+console.log("Null:- null is something but not zero or undefined. It is required to assign null but not undefined");
+console.log('====================== Undefined==================')
+var height;
+console.log('Height:-',height,"           ", "Variable Type:-",typeof height);
+console.log("Undefined:- undefined garbage value stored in variable after declaration only.")
+
+
