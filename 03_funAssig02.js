27cf9d9dffc457cfcfac71c1cdf941232b1eb77a
shingadeanandrao <shingadeanandrao@gmail.com>
2023-09-25 20:27:33 +0530

    String Basics Assignment
    String Basics Assignment

---
+++ 03_funAssig02.js
@@ -0,0 +1,7 @@
+console.log("=================Assignment 1: Bank Details==============");
+function bankDetails(bankName,accountNum,location,pinCode){
+    console.log("This is the account details for:- Bank Name:",bankName,"   ","Account Number:",accountNum,"   ","City:",location,"   ","Pincode:",pinCode);
+}
+bankDetails("CITI Bank",3456782345,'Pune',431202);
+bankDetails("Axis Bank",7856782345,'Mumbai',441202);
+bankDetails("HDFC Bank",8956782345,'Pune',631202);
---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,17 @@
+console.log("=================Assignment 2: Hobbies==============");
+function stringBasics(){
+    console.log('My Dream Company is Infosys');
+    console.log("================================================");
+    console.log("My Hobbies are as following:");
+    var hobby1="Exploring new Technology";
+    var hobby2="Traveling";
+    var hobby3="Farming";
+    console.log("My Hobbies are==>",hobby1," ",hobby2," ",hobby3);
+    console.log("================================================");
+    var result1= hobby1.length;
+    var result2= hobby2.length;
+    var result3= hobby3.length;
+    var totalNumOfChar=result1+result2+result3;
+    console.log("Total Number of characters in hobbies are:-",totalNumOfChar);
+}
+stringBasics();
