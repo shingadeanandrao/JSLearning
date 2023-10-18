5b7bf35c6ee2640649c1ea3392647f7bfb6cf65f
shingadeanandrao <shingadeanandrao@gmail.com>
2023-10-18 21:22:13 +0530

    removed Duplicate element from array
    removed Duplicate element from array

---
+++ 15_higherOrderFunction.js
@@ -0,0 +1,34 @@
+//function that is taking another function as arg that function is also returning another function.
+
+function jennyHomework(callback){
+
+    console.log(`Jenny started assignments`);
+
+    console.log('Her assignments solving is in progress');
+
+    console.log(`Jenny's assignment is completed`);
+    console.log(`====================================================`);
+    callback();
+    
+    return function(){
+        console.log(`Yahoo, We both completed assignments.`);
+    }
+
+}
+
+let elonCopyHomework = function(){
+
+    console.log(`Thank you Jenny for calling me back`);
+
+    console.log('Elon started copying assignments');
+
+    console.log('Elon copied all assignments');
+    console.log(`====================================================`);
+
+}
+
+let result = jennyHomework(elonCopyHomework);
+
+result();
+
+ 
---
+++ 18_strictMode.js
@@ -0,0 +1,12 @@
+'use strict'
+// firstName = 'Jenny ; // Not allowed in Strict mode
+
+// console.log(firstName);
+'use strict'
+
+person = {
+    fullname :'Billgates',
+    age : 67
+}
+console.log(person);
+delete person;
